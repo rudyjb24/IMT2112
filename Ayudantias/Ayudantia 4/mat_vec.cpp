7d69d72e7916183423dd6c1f4c9805580b37bcde
rjerez1 <rjerez1@uc.cl>
2019-09-23 11:29:57 -0300

    Clases 10-11-12

--- Ayudantias/Ayudantia 4/mat_vec.cpp
+++ Ayudantias/Ayudantia 4/mat_vec.cpp
@@ -5,6 +5,7 @@
 
 float **matrix_generator(int n, int range)
 {
+  srand((int)time(0));
   float **matrix = (float **) malloc(n * sizeof(float *));
   for (int i = 0; i < n; i++)
   {
@@ -78,23 +79,43 @@ float *mat_vec(float **matrix, float *vector, int n)
   return result;
 }
 
+float *mat_vec_par(float **matrix, float *vector, int n)
+{
+    float *result = (float *)calloc(n, sizeof(float));
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i] += matrix[i][j] * vector[j];
+        }
+    }
+  return result;
+}
+
 int main() {
   
-  int n = 3;
-  clock_t start, end;
+  int n = 10000;
+  clock_t start, end, start2, end2;
 
-  start = clock();
+  start = time(0);
   float **A = matrix_generator(n, 10);
   float *x = vector_generator(n, 10);
   float *b = mat_vec(A, x, n);
-  end = clock();
+  end = time(0);
 
-  print_matrix(A, n);
-  print_vector(x, n);
-  print_vector(b, n);
 
-  printf("\nNos demoramos %2f seg\n", (double)(end-start)/CLOCKS_PER_SEC);
+  //print_matrix(A, n);
+  //print_vector(x, n);
+  //print_vector(b, n);
+
   
+  start2 = time(0);
+  float *bp = mat_vec_par(A, x, n);
+  end2 = time(0);
+
+  printf("\nNos demoramos %2f seg\n", (double)(end-start));
+  printf("\nNos demoramos %2f seg\n", (double)(end2-start2));
   free_matrix(A, n);
   free(x);
   return 0;
