1ecdb03f854a1bbbc653dc76d2edee9a849835b6
rjerez1 <rjerez1@uc.cl>
2019-10-22 18:32:39 -0300

    Clases 19 + 20

--- Ayudantias/MPI 2/job.sh
+++ Ayudantias/MPI 2/job.sh
@@ -5,9 +5,8 @@
 #SBATCH --job-name=IMT2112
 #SBATCH --output=log.out
 
-#SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=1
 
 mpic++ producto_punto.cpp
-mpirun a.out
+mpirun --mpi=openmpi a.out
